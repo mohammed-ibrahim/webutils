d42831754ca7f6e1c2d9cc12bcb7c5b32107b5f5
mohammed-ibrahim <mohammed.ibrahim@practo.com>
2018-04-07 14:49:34 +0530

    Adding timestamps

--- util.js
+++ util.js
@@ -11,6 +11,42 @@ window.onload = function () {
 };
 
 
+function readableTimeStamp() {
+  var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+  var dayOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+  //format: mon.31.jan.2019.21.20
+  var date = new Date();
+  var day = dayOfWeek[date.getDay()];
+  var dt = date.getDate().toString();
+  var month = months[date.getMonth()];
+  var year = date.getFullYear().toString();
+
+  return day.toString()
+      + "."
+      + dt
+      + "."
+      + month.toString()
+      + "."
+      + year.toString()
+      + "."
+      + date.getHours().toString()
+      + "."
+      + date.getMinutes().toString()
+      + "."
+      + date.getSeconds().toString();
+}
+
+function applyNewTimeStamp() {
+  var ts = readableTimeStamp()
+  document.getElementById("ts_txt_holder").value = ts;
+  document.getElementById("ts_txt_holder").setSelectionRange(0, ts.length)
+}
+
+function applyUnixNewTimeStamp() {
+  var ts = new Date().getTime().toString()
+  document.getElementById("unix_ts_txt_holder").value = ts;
+  document.getElementById("unix_ts_txt_holder").setSelectionRange(0, ts.length)
+}
 
 function bytesToMb() {
   var txtBytes = document.getElementById("bytes_holder").value;
